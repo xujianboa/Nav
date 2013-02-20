912a1260162cc70fbe48861bd829d17b3ebeab15
Ricky <ldci@xujianbo.local>
2013-02-20 15:09:52 +0800

    Initial Commit

---
+++ Nav/NavAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NavAppDelegate.h
+//  Nav
+//
+//  Created by Ricky on 13-2-20.
+//  Copyright (c) 2013年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nav
+//
+//  Created by Ricky on 13-2-20.
+//  Copyright (c) 2013年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NavAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NavAppDelegate class]));
+    }
+}
